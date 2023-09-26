f34839543189902c6a6169844ca6feeb07790561
Astrid <35829@ma-web.nl>
2023-09-26 10:20:11 +0200

    set up

---
+++ mymusiclibrary/public/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>My Music Library</title>
+</head>
+<body>
+    <header>
+        <h1>My Music Library</h1>
+        <div>
+            <button>Algemene Voorwaarden</button>
+            <button>Systeem Informatie</button>
+        </div>
+    </header>
+    <main>
+        <section class="songs">
+            <nav>
+                <ul>
+                    <li class="song">
+                        <p class="song__name">Angels</p>
+                        <p class="song__artist">All Good Things</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Supernova</p>
+                        <p class="song__artist">From Fall To Spring</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Echo</p>
+                        <p class="song__artist">Starset</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Supercollide
+                        </p>
+                        <p class="song__artist">BANNNERS</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Weight of the world</p>
+                        <p class="song__artist">Self Deception</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Madhouse</p>
+                        <p class="song__artist">Citizen Soldier</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Dull knives</p>
+                        <p class="song__artist">Imagine Dragons</p>
+                    </li>
+                    <li class="song">
+                        <p class="song__name">Save Me</p>
+                        <p class="song__artist">Bruno Martini, AVIAN GRAYS, Mayra, TRIXL</p>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <section class="search">
+            <input type="text" placeholder="Search for a product here..." class="searchBar__input">
+            <button>Search</button>
+        </section>
+    </main>
+</body>
+</html>
+
+<?php
+    for($i=0; $i<10; $i++){
+        //haal hier de view/card.php op
+    }
+?>
